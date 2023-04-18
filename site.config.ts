a2f458548e5691314eefd6b86df13a93fab8a6f7
Tian <tianlijun315@gmail.com>
2023-04-18 10:31:16 +0000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'K8S-a7ef3336fdbb4dbe91c3a9ab04a8b7e6',
+  // https://tianlj.notion.site/
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
